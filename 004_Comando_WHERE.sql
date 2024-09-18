d6cf1f895ef13dbd70d0b5c4ea468d6d9c5578fb
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-09-18 17:25:52 -0500

    Ejecucion Comando WHERE

---
+++ 004_Comando_WHERE.sql
@@ -0,0 +1,17 @@
+/*Comando WHERE*/
+
+/*Este comando nos devuelve a todos los usuarios con la edad = 15*/
+SELECT * FROM usuarios WHERE edad = 15;
+
+/*Comando para filtrar todos las columnas name donde la edad es igual = 15*/
+SELECT name FROM usuarios WHERE edad = 15;
+
+/*Comando para filtrar todos las columnas name donde la edad es igual = 15*/
+SELECT name FROM usuarios WHERE edad = 15;
+
+/*Comando para filtrar todos los nombre distintos de la tabla 'ususarios' con edad = 15*/
+SELECT DISTINCT name FROM usuarios WHERE edad = 15;
+
+/*Comando para filtrar las edad distintas de la tabla 'ususarios' con edad = 15*/
+SELECT DISTINCT edad FROM usuarios WHERE edad = 15;
+
