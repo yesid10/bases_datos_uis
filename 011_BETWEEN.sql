75f57642aaa5f3e6bf57a8df1a369e87a46e2293
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-09-25 08:01:08 -0500

    comando min and max

---
+++ 011_BETWEEN.sql
@@ -0,0 +1,5 @@
+--Comando que nos ordena y nos devuelve los datos segun los randos dados
+
+-- Comando para ordernar todos los datos de la tabla "usuarios" con edad comprendidad entre 
+-- los intervalos correspondientes, en este caso entre 11 a 61
+SELECT * FROM usuarios WHERE edad BETWEEN 11 AND 61
---
+++ 012_MIN_MAX.sql
@@ -0,0 +1,7 @@
+--Estos comandos nos permiten filtrar por un valor maximo o un valor minimo de una columna
+
+--Obtiene el valor minimo en el campo de la edad de la tabla "usuarios"
+SELECT MIN(edad) FROM usuarios;
+
+--Obtiene el valor maximo en el campo de la edad de la tabla "usuarios"
+SELECT MAX(edad) FROM usuarios;
