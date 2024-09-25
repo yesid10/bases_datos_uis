81be760fd08bd08e6a9e374e5150ae207b25dec3
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-09-25 08:41:39 -0500

    Comando IN

---
+++ 013_IN.sql
@@ -0,0 +1,5 @@
+--El comando IN es utilizado para hacer consultas dependiende de la condicion
+
+--Comando para ordenar todos los datos de la tabla "usuarios" con nombre 
+--brais y sara
+SELECT * FROM usuarios WHERE name IN ('luis', 'juan');
